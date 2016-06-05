31565e342a0f2b0e23f0bb2eeccffe9667c2d921
artur199209 <arti190@gmail.com>
2016-06-05 20:34:08 +0200

    Refaktoryzacja

---
+++ DuzyLotek/Form1_UpdateLabel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DuzyLotek
+{
+    public partial class Form1:Form, ILotto
+    {
+        public void UpdateLabel()
+        {
+            try
+            {
+                ControlExtensions.InvokeIfRequired(lastScoresLabel, delegate {
+                    lastScoresLabel.Text = "Ostatnie wyniki:";
+                    lastScoresLabel.Visible = true;
+                    lastScoresDate.Text = LastScores.Date2;
+                    lastScores.Text = LastScores.Scores;
+                });
+            }
+            catch (Exception ex) { }
+       }
+    }
+}
---
+++ DuzyLotek/Form1_randomScores.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuzyLotek
+{
+   public partial class Form1:Form, ILotto
+    {
+       public string randomScores(int n, int k)
+       {
+           Random rand = new Random();
+           string sc = "";
+           int[] numbers = new int[n];
+		   for (int i = 0; i < n; i++)
+		   {
+			   numbers[i] = i + 1;
+		   }
+
+           for (int i = 0; i < k; i++)
+           {
+               int r = rand.Next(n);
+               sc = sc + numbers[r] + " ";
+               numbers[r] = numbers[n - 1];
+               n--;
+           }
+           return sc;
+       }
+    }
+}
